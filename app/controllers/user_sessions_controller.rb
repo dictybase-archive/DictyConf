f95bc891d4ae41db8c53017816d118ed626a7e80
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-03-25 22:15:01 -0500

    Controller code for user & session management

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,43 @@
 class UserSessionsController < ApplicationController
+
+	before_filter :require_no_user, :only => [:new, :create]
+	before_filter :require_user, :only => :destroy
+
+	include UserSessionsHelper
+
+	def new
+		@user_session = UserSession.new
+	end
+
+	def create
+		@user = get_user(params[:user_session])
+
+		has_valid_email = valid_email?(params[:user_session][:email])
+		has_valid_password = verify_recaptcha
+
+		if !has_valid_email or !has_valid_password
+			logger.debug "Invalid email/password"
+			render :action => :new
+		elsif @user.nil?
+			logger.debug "User doesn't exist"
+			render :signup
+		else
+			@user_session = UserSession.new(@user)
+			if @user_session.save
+				logger.info "Login succesful"
+				flash[:notice] = "Login successful!"
+				redirect_back_or_default :home
+			else
+				logger.debug "Login failed"
+				flash[:notice] = "Login failed!"
+				render :action => :new
+			end
+		end
+	end
+
+	def destroy
+		current_user_session.destroy
+		flash[:notice] = "Logout successful!"
+		redirect_back_or_default new_user_session_url
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+
+	before_filter :require_no_user, :only => [:new, :create]
+	before_filter :require_user, :only => [:show, :edit, :update]
+	#skip_before_filter :login_required, :only => [:new,:create]
+
+	def new
+		email = params[:user_session][:email] if ! params[:user_session].nil?
+		email ||= params[:email]
+		#password = params[:user_session][:password] if ! params[:user_session].nil?
+		@user = User.new(:email => email )
+
+		respond_to do |format|
+			format.html # new.html.erb
+		end
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:notice] = "Account registered!"
+			redirect_back_or_default :home
+		else
+			render :action => :new
+		end
+	end
+
+	def show
+		@user = @current_user
+	end
+
+	def edit
+		@user = @current_user
+	end
+
+	def update
+		@user = @current_user
+		if @user.update_attributes(params[:user])
+			flash[:notice] = "Account updated!"
+			redirect_to :home
+		else
+			render :action => :edit
+		end
+	end
 end
