b46f4a5d98c4d01037ab23492cd486f3cb363461
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-03-25 22:15:28 -0500

    Helper methods for user & session management

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -1,2 +1,12 @@
 module UserSessionsHelper
+
+	def get_user(the_user)
+		User.find_by_email(the_user[:email])
+	end
+
+	def valid_email?(email)
+		reg = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
+		return (reg.match(email))? true : false
+	end
+
 end
