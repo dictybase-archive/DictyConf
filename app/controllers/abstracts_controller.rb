6014ba941cc9eaf78283f75270a80474dee7bf2e
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-03-27 15:09:29 -0500

    Adbstracts controller with index & show actions

--- app/controllers/abstracts_controller.rb
+++ app/controllers/abstracts_controller.rb
@@ -1,2 +1,30 @@
 class AbstractsController < ApplicationController
+	skip_before_filter :login_required, :only => [ :index, :show ]
+
+	def index
+		logger.debug "########## #{params[:user_id]}"
+		if params[:user_id]
+			@abstracts = Abstract.find(:all, :conditions =>[:user_id => params[:user_id] ])
+		else
+			@abstracts = Abstract.all
+		end
+		respond_to do |format|
+			format.html # index.html.erb
+			#format.xml { render :xml => @abstracts }
+		end
+	end
+
+	def show
+		logger.debug "***** #{params[:id]}"
+		@abstract = Abstract.find(params[:id])
+		#@abstract = Abstract.where(:user_id => current_user.id).find(params[:id])
+		respond_to do |format|
+			format.html # show.html.erb
+			#format.xml { render :xml => @abstract }
+		end
+	end
+
+	def edit
+
+	end
 end
